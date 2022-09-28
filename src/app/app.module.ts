aba49afe4a6d865efce04a0ec98941f39cb69275
Duaa Mohammed <62574473+duuaa@users.noreply.github.com>
2022-09-28 11:27:44 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { OrderService } from './components/services/order-service/order.service';
+import { ProductService } from './components/services/product-service/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    OrdersComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    OrderService,
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
