25582e871eb9e63d389027d8243496c13d498ab3
Duaa Mohammed <62574473+duuaa@users.noreply.github.com>
2022-09-28 14:08:32 +0200

    Create URLs.ts

---
+++ src/app/components/services/utils/URLs.ts
@@ -0,0 +1,5 @@
+import { environment } from "src/environments/environment";
+
+export const usersAPI = environment.APIURL + "/users"
+export const productsAPI = environment.APIURL + "/products"
+export const ordersAPI = environment.APIURL + "/orders"
