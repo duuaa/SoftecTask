e0d9930658366eb83a5030ce078dcb67442e08e9
Duaa Mohammed <62574473+duuaa@users.noreply.github.com>
2022-09-29 18:52:00 +0200

    impl order details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SaveComponent } from './components/save/save.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'orders/:id',     component: OrderDetailsComponent },
+  { path: 'save-order',     component: SaveComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/model/dto.model.ts
+++ src/app/model/dto.model.ts
@@ -1,34 +1,36 @@
 export interface ProductDto{
-  id: number,
-  ProductName: string,
-  ProductPrice: number,
-  AvailablePieces: number,
-  ProductImg: string
+  id: number;
+  ProductName: string;
+  ProductPrice: number;
+  AvailablePieces: number;
+  ProductImg: string;
 }
 
 export interface OrderDto{
-  id: number,
-  OrderDate: string,
-  UserId: string,
-  Products: ProductOrderDto[],
-  PaymentType: string
-}
+  id: number;
+  OrderDate: string;
+  UserId: string;
+  Products: ProductOrderDto[];
+  PaymentType: string;
+  total?: number;
+  productsItems: ProductDto[];
+  }
 export interface AddOrderRequestDto{
-  OrderDate: string,
-  UserId: string,
-  Products: ProductOrderDto[],
-  PaymentType: string
+  OrderDate: string;
+  UserId: string;
+  Products: ProductOrderDto[];
+  PaymentType: string;
 }
 
 export interface ProductOrderDto{
-  ProductId: number,
-  Quantity: number
+  ProductId: number;
+  Quantity: number;
 }
 export interface UserDto{
-  id: string,
-  Name: string,
-  Email: string,
-  Phone: string,
-  Address: string,
-  RegisterDate: string
+  id: string;
+  Name: string;
+  Email: string;
+  Phone: string;
+  Address: string;
+  RegisterDate: string;
 }
