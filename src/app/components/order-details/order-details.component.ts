d09d43640a750c000fcf52a9a84a81fcbed481f9
Duaa Mohammed <62574473+duuaa@users.noreply.github.com>
2022-09-30 12:43:44 +0200

    handle subscriptions to be un subscribed OnDestroy

--- src/app/components/order-details/order-details.component.ts
+++ src/app/components/order-details/order-details.component.ts
@@ -2,9 +2,9 @@ import { ProductService } from './../../services/product-service/product.service
 import { UserDto, ProductDto } from './../../model/dto.model';
 import { OrderDto } from 'src/app/model/dto.model';
 import { OrderService } from './../../services/order-service/order.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
@@ -12,33 +12,41 @@ import { UserService } from 'src/app/services/user-service/user.service';
   templateUrl: './order-details.component.html',
   styleUrls: ['./order-details.component.css']
 })
-export class OrderDetailsComponent implements OnInit {
+export class OrderDetailsComponent implements OnInit, OnDestroy {
   order: OrderDto;
   user: UserDto;
   products: ProductDto[] = [];
+  componentSubscripitions: Array<Subscription> = [];
+
   constructor(private route: ActivatedRoute,
               private orderService: OrderService,
               private userService: UserService,
               private productService: ProductService,
               private router : Router) { }
 
-  ngOnInit() {
-
-    this.getOrderItem();
+  ngOnDestroy(): void {
+    this.componentSubscripitions.forEach((subscription: Subscription)=>{
+      subscription.unsubscribe();
+    })
   }
-  getOrderItem(){
-    this.route.params.pipe(switchMap((params: Params) =>
+  ngOnInit() {
+    this.componentSubscripitions.push(this.getOrderItem());
+  }
+  getOrderItem(): Subscription{
+    return this.route.params.pipe(switchMap((params: Params) =>
     this.orderService.getOrderById( params['id'])
     )).subscribe((orderResponse)=>{
         this.order = orderResponse
-        this.productService.getProducts().subscribe((response)=>{
+         this.componentSubscripitions.push(
+          this.productService.getProducts().subscribe((response)=>{
           this.products= response;
           this.order.productsItems = this.products.filter(product=>
             this.order.Products.find(p=> p.ProductId == product.id))
-        })
+        }));
+        this.componentSubscripitions.push(
         this.userService.getUserById(this.order.UserId).subscribe((userResponse)=>{
           this.user = userResponse
-        });
+        }));
 
       })
 
